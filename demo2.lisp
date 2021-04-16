f0f60af5b8ad5c0d5b450e706f4528771589f31f
Joel Ward <jmward@gmail.com>
2021-04-16 14:03:59 -0500

    update

--- demo2.lisp
+++ demo2.lisp
@@ -12,6 +12,8 @@
 (defpackage :my-chat #.(append '(:use :cl) *package-list*)
 (in-package :my-chat)
 
+(setq *attribute-quote-char* #\")
+  
 (defclass chat-room (hunchensocket:websocket-resource)
   ((name :initarg :name :initform (error "Name this room!") :reader name))
   (:default-initargs :client-class 'user))
@@ -67,3 +69,52 @@
 ;; enter `ws://localhost:12345/bongo` as the host and play around chatting with
 ;; yourself.
 
+;; ;; Examples
+
+;; The ps macro takes Parenscript code in the form of s-expressions (Parenscript code and Common Lisp code share the same representation), translates as much as it can into constant strings at macro-expansion time, and expands into a form that will evaluate to a string containing JavaScript code.
+
+(hunchentoot:define-easy-handler (example1 :uri "/example1") ()
+  (with-html-output-to-string (s)
+    (:html
+     (:head (:title "Parenscript tutorial: 1st example"))
+     (:body (:h2 "Parenscript tutorial: 1st example")
+            "Please click the link below." :br
+            (:a :href "#" :onclick (ps (alert "Hello World"))
+                "Hello World")))))
+
+;; One way to include Parenscript code in web pages is to inline it in HTML script tags:
+
+(hunchentoot:define-easy-handler (example2 :uri "/example2") ()
+  (with-html-output-to-string (s)
+    (:html
+     (:head
+      (:title "Parenscript tutorial: 2nd example")
+      (:script :type "text/javascript"
+               (str (ps
+                      (defun greeting-callback ()
+                        (alert "Hello World"))))))
+     (:body
+      (:h2 "Parenscript tutorial: 2nd example")
+      (:a :href "#" :onclick (ps (greeting-callback))
+          "Hello World")))))
+
+;; Another way to integrate Parenscript into a web application is to serve the generated JavaScript as a separate HTTP resource. Requests to this resource can then be cached by the browser:
+
+(hunchentoot:define-easy-handler (example3 :uri "/example3.js") ()
+  (setf (hunchentoot:content-type*) "text/javascript")
+  (ps
+    #| start javascript |#
+    (defun find-location (path)
+      (if path
+	  (concatenate 'string "ws" (location.origin.substr 4) path)
+	  (concatenate 'string "ws" (location.href.substr 4))))
+    (defun make-websocket (url)
+      (-web-socket url)
+      (alert "Hello World"))
+    (defun greeting-callback ()
+      (alert "Hello World"))
+    (defun greeting-callback2 ()
+      (alert "Hello World2"))
+    (defun greeting-callback3 ()
+      (alert "Hello World3"))
+    #| end javascript |#))
